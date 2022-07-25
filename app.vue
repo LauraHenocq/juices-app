5937d9ab06a6883d75002dab516340d9fb7bfbd1
Laura Hénocq <laura.henocq@gmail.com>
2022-07-25 18:20:43 +0200

    :sparkles: Ajoute le menu et le logo animé de l'app

--- app.vue
+++ app.vue
@@ -1,33 +1,25 @@
+<script setup lang="ts">
+const menuToDisplay: Array = useState('menu', () => [
+  {
+    label: 'Les recettes de jus',
+    link: '/'
+  },
+  {
+    label: 'Mes favoris',
+    link: '/favoris'
+  },
+  {
+    label: 'Les fruits et légumes de saisons',
+    link: '/saison'
+  }
+])
+</script>
 <template>
   <div class="homepage container">
-    <h1 class="homepage__title">
+    <CommonMenu :menu="menuToDisplay" />
+    <CommonTitle>
       Mes recettes de jus
-    </h1>
+    </CommonTitle>
     <a href="https://www.flaticon.com/fr/icones-gratuites/regime" title="régime icônes">Régime icônes créées par Freepik - Flaticon</a>
   </div>
 </template>
-<style scoped lang="scss">
-.homepage__title {
-  letter-spacing: 5px;
-  position: relative;
-  text-align: center;
-  background: linear-gradient(90deg, $secondary 0%, $primary 100%);
-  background-clip: initial;
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-}
-
-.homepage__title::after {
-  position: absolute;
-  bottom:-10px;
-  display: inline-block;
-  width: 60px;
-  height: 60px;
-  content: '';
-  background-image: url('~/assets/icons/logo.png');
-  background-repeat: no-repeat;
-  background-size: contain;
-  background-position: bottom;
-  margin-left: 10px;
-}
-</style>
